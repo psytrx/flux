562cc39d87b5a381e6fcdc94b36d2bf7bc09a80b
psytrx <fj.dumont@gmail.com>
2023-11-21 10:39:50 +0100

    add logging & tracing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
     let resolution = glam::uvec2(512, 512);
-    let img = image::Rgb32FImage::from_fn(resolution.x, resolution.y, |x, y| {
-        let r = x as f32 / resolution.x as f32;
-        let g = y as f32 / resolution.y as f32;
-        let b = 0.0;
-        image::Rgb([r, g, b])
-    });
-    img.save("./output/output.exr")?;
+
+    let img = {
+        measure_time::trace_time!("drawing image");
+        image::Rgb32FImage::from_fn(resolution.x, resolution.y, |x, y| {
+            let r = x as f32 / resolution.x as f32;
+            let g = y as f32 / resolution.y as f32;
+            let b = 0.0;
+            image::Rgb([r, g, b])
+        })
+    };
+
+    {
+        measure_time::trace_time!("saving image");
+        img.save("./output/output.exr")?;
+    }
+
     Ok(())
 }
