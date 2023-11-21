e50d6d3fcf7eb61aa7935c995185a7d13b4d8033
psytrx <fj.dumont@gmail.com>
2023-11-21 10:34:06 +0100

    create & write an image

---
+++ output/README.md
@@ -0,0 +1,3 @@
+# Output Directory
+
+Output files will, by default, be written to this directoy.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    let resolution = glam::uvec2(512, 512);
+    let img = image::Rgb32FImage::from_fn(resolution.x, resolution.y, |x, y| {
+        let r = x as f32 / resolution.x as f32;
+        let g = y as f32 / resolution.y as f32;
+        let b = 0.0;
+        image::Rgb([r, g, b])
+    });
+    img.save("./output/output.exr")?;
+    Ok(())
 }
