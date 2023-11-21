7acf16f4077c704beb83c9445a603560beb6c77f
psytrx <fj.dumont@gmail.com>
2023-11-21 12:26:54 +0100

    add ray-sphere intersection

--- src/flux/mod.rs
+++ src/flux/mod.rs
@@ -22,7 +22,27 @@ pub fn render_film(resolution: glam::UVec2) -> Film {
 }
 
 fn ray_color(ray: Ray) -> glam::Vec3 {
+    if hit_sphere(&ray) {
+        glam::Vec3::X
+    } else {
+        background(ray)
+    }
+}
+
+fn background(ray: Ray) -> glam::Vec3 {
     let unit_direction = ray.direction.normalize();
     let a = 0.5 * (unit_direction.y + 1.0);
     (1.0 - a) * glam::Vec3::ONE + a * glam::vec3(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(ray: &Ray) -> bool {
+    let center = glam::Vec3::ZERO;
+    let radius = 1.0;
+
+    let oc = ray.origin - center;
+    let a = ray.direction.dot(ray.direction);
+    let b = 2.0 * oc.dot(ray.direction);
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
 }
